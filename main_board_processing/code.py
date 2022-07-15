c2ad55cd6d893c925f03af7cd1ae9f9a23c37110
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-07-15 22:43:14 +0100

    update

--- main_board_processing/code.py
+++ main_board_processing/code.py
@@ -1,15 +1,23 @@
 import blue_square
-import red_circle
+import spotigram
 
 import board
+import busio
 import digitalio
 from adafruit_matrixportal.matrix import Matrix
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+import adafruit_requests as requests
+from digitalio import DigitalInOut
 
 def main():
 
     display = get_matrix_display()
     display_image = None
+    
+    connect_to_wifi()
 
+    # modes: {"CLOCK", "INSTAGRAM", "TFL", "FLIGHTS"}
     mode = None
 
     count = 0
@@ -18,10 +26,10 @@ def main():
         # Request an API & get the current state
         # If the mode has been updated, update the mode state of the app. This will determine which route below is called.
 
-        if count % 2 == 0:
+        if count == 10000:
             display_image = blue_square.say_hello()
         else:
-            display_image = red_circle.red_circle()
+            display_image = spotigram.get_album_artwork()
 
         count += 1
 
@@ -31,6 +39,31 @@ def main():
 def get_matrix_display():
     matrix = Matrix(width=32, height=32, bit_depth=6)
     return matrix.display
+    
+def connect_to_wifi():
+    esp32_cs = DigitalInOut(board.ESP_CS)
+    esp32_ready = DigitalInOut(board.ESP_BUSY)
+    esp32_reset = DigitalInOut(board.ESP_RESET)
+
+    spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+    requests.set_socket(socket, esp)
+
+    try:
+        from secrets import secrets
+    except ImportError:
+        print("Secrets files were not found")
+        raise
+
+
+    print("Connecting to AP...")
+    while not esp.is_connected:
+        try:
+            esp.connect_AP(secrets["ssid"], secrets["password"])
+        except OSError as e:
+            print("could not connect to AP, retrying: ", e)
+            continue
 
 if __name__ == '__main__':
     main()
---
+++ main_board_processing/spotigram.py
@@ -0,0 +1,77 @@
+from adafruit_display_shapes.rect import Rect
+import adafruit_requests as requests
+import adafruit_imageload
+import displayio
+import time
+
+count = 0
+
+def get_album_artwork():
+    current_album_title = ""
+    current_album_title_url = "https://pi-screen.herokuapp.com/currentAlbum"
+    get_current_album_artwork_url = "https://pi-screen.herokuapp.com/"
+
+    if user_is_listening_to_song() == True:
+
+        response = requests.get("https://pi-screen.herokuapp.com/currentAlbum").text
+
+        if response == current_album_title:
+            print("nothing to change")
+            time.sleep(5)
+            return render_board_with_artwork("/art.bmp")
+        # render the board with the artwork as an argument.
+        print("Fetching album artwork")
+        r = requests.get("https://pi-screen.herokuapp.com/")
+
+        with open("art.bmp", 'wb') as handler:
+            handler.write(r.content)
+
+        current_album_title = get_new_album_title()
+
+        return render_board_with_artwork("/art.bmp")
+
+    else:
+        # render the board with the placeholder as an argument
+        if current_album_title == "placeholder":
+            print("nothing to change")
+            time.sleep(5)
+            return render_board_with_artwork("/placeholder.bmp")
+        print("rendering placeholder")
+        r = requests.get("https://pi-screen.herokuapp.com/instagram")
+        current_album_title = "placeholder"
+        with open("placeholder.bmp", 'wb') as handler:
+            handler.write(r.content)
+
+        return render_board_with_artwork("/placeholder.bmp")
+
+    return
+
+def user_is_listening_to_song():
+    response = requests.get("https://pi-screen.herokuapp.com/currentAlbum")
+    return False if response.status_code == 500 else True
+
+def get_new_album_title():
+    response = requests.get("https://pi-screen.herokuapp.com/currentAlbum")
+
+    return response.text
+
+def render_board_with_artwork(file_location):
+    image_bit, image_pal = adafruit_imageload.load(file_location,
+                                                     bitmap=displayio.Bitmap,
+                                                     palette=displayio.Palette)
+    # bitmap & pallet above are used to create the grid of individual
+    # tiles, 32 x 32, cut from the long bitmap image named in the
+    # quotes, above
+    image_grid = displayio.TileGrid(image_bit, pixel_shader=image_pal,
+                                     width=1, height=1,
+                                     tile_height=32, tile_width=32,
+                                     default_tile=0,
+                                     x=0, y=0)
+
+    group = displayio.Group()
+    group.append(image_grid)
+
+
+    print(group)
+    return group
+
